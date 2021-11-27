2ac34a9b3f36d2fe6db2cb02143e33b59b7d65ca
Axhar85 <azhararshad_ntu@yahoo.com>
2021-11-27 10:16:11 +0100

    adding data woith postman

--- src/server.js
+++ src/server.js
@@ -6,5 +6,7 @@ app.use(bodyparser.json());
 
 app.get('/hello', (req, res) => res.send("Hello"));
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`));
+app.get('./hello/:name', (req, res) => res.send (`Hello ${req.params.name}`));
+
 
 app.listen(8000, () => console.log("Listening on port 8000"));
