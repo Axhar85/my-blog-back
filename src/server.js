bc73e4f7113f756ba239f6a9d842da84bf460b9e
Axhar85 <azhararshad_ntu@yahoo.com>
2021-11-29 09:56:49 +0100

    connecting with db

--- src/server.js
+++ src/server.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const bodyparser = require("body-parser");
-
-const articlesInfo = {
-    "learn-react": {
-        comments: [],
-    },
-    "learn-node": {
-        comments: [],
-    },
-
-    "my-thoughts-on-learning-react": {
-        comments: [],
-    },
-
-
-
-}
+const {MongoClient} = require("mongodb")
 
 const app = express();
 app.use(bodyparser.json());
+
+app.get('/api/articles/:name', async(req, res) => {
+    try {
+    const articleName = req.params.name;
+    const client = await MongoClient.connect('mongodb://localhost:27017', {
+        useNewUrlParser : true,
+        useUnifiedTopology: true,
+    });
+    const db = client.db("myblog");
+    const articlesInfo = await db.collection('articles').findOne({name : articleName})
+        res.status(200).json(articlesInfo);
+        client.close();
+    } catch(error) {
+        res.status(500).json({message: 'Error connecting to db', error});
+    }
+});
 
 app.post("/api/articles/:name/add-comments", (req, res) => {
     const {username, text} = req.body;
